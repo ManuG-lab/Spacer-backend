595033a984910796b86a0721fe3dd35c9f53ba58
Emmanuel Gitau <gmannu2055@gmail.com>
2025-08-01 13:54:27 +0300

    cloudinary

--- routes/spaces_routes.py
+++ routes/spaces_routes.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import db, Space, User
-import cloudinary
+import cloudinary.uploader
 import cloudinary.uploader
 from dotenv import load_dotenv
 import os
